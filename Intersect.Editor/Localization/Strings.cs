aacead895d45b47808ce05cbe7e2c6740314a9de
Joyce <azurebeats@hotmail.nl>
2020-05-16 17:49:13 +0200

    Hair Patch
    Signed-off-by: xHashii <59124311+xHashii@users.noreply.github.com>

--- Intersect.Editor/Localization/Strings.cs
+++ Intersect.Editor/Localization/Strings.cs
@@ -857,6 +857,10 @@ Tick timer saved in server config.json.";
 
             public static LocalizedString undotitle = @"Undo Changes";
 
+            public static LocalizedString hairstyles = @"Hairstyles";
+
+            public static LocalizedString hair = @"Hair:";
+
         }
 
         public struct Colors
@@ -1166,6 +1170,19 @@ Tick timer saved in server config.json.";
             public static LocalizedString okay = @"Ok";
 
             public static LocalizedString title = @"Change Face";
+
+        }
+
+        public struct EventChangeHair
+        {
+
+            public static LocalizedString cancel = @"Cancel";
+
+            public static LocalizedString label = @"Hair:";
+
+            public static LocalizedString okay = @"Ok";
+
+            public static LocalizedString title = @"Change Hair";
 
         }
 
@@ -1594,6 +1611,8 @@ Tick timer saved in server config.json.";
 
             public static LocalizedString setface = @"Set Player Face to {00}";
 
+            public static LocalizedString sethair = @"Set Player Hair to {00}";
+
             public static LocalizedString setnamecolor = @"Set Player Name Color";
 
             public static LocalizedString removenamecolor = @"Remove Player Name Color";
@@ -1728,6 +1747,7 @@ Tick timer saved in server config.json.";
                 {"changenamecolor", @"Change Name Color"},
                 {"inputvariable", @"Input Variable"},
                 {"changeplayerlabel", @"Change Player Label"},
+                {"changehair", @"Change Hair"},
             };
 
         }
